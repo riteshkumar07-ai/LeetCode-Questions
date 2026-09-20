75e0036a872d5d83c76bc1236f83ac8a41656909
Ritesh Kumar <riteshkumar972116@gmail.com>
2026-09-20 19:55:03 +0530

    Add removeElement function for LeetCode 27
    Implemented the removeElement function using a two-pointer approach to remove specified elements from an array.

--- Arrays/0027_Remove_Element.c
+++ Arrays/0027_Remove_Element.c
@@ -1,1 +1,20 @@
+/*
+    LeetCode 27 - Remove Element
 
+    Approach: Two Pointer
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
+int removeElement(int* nums, int numsSize, int val) {
+    int k = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
