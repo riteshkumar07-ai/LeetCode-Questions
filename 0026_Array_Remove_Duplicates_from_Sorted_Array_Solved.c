177ba49c2d6f8f2bebc36674f63cd0dfdd5bcfa9
Ritesh Kumar <riteshkumar972116@gmail.com>
2026-09-22 22:59:27 +0530

    Delete 0026_Array_Remove_Duplicates_from_Sorted_Array_Solved.c

--- 0026_Array_Remove_Duplicates_from_Sorted_Array_Solved.c
+++
@@ -1,9 +0,0 @@
-int removeDuplicates(int* nums, int numsSize) {
-    int i = 0;
-    for (int j = 1; j < numsSize; j++) {
-        if (nums[i] != nums[j]) {
-            nums[++i] = nums[j];
-        }
-    }
-    return i + 1;
-}
